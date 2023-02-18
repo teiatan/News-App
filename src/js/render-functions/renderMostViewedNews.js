bfdf7fe877c2c868fbf9d4a8a5a43c361b64a4e0
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-02-18 15:56:36 +0200

    alt img 33 task

--- src/js/render-functions/renderMostViewedNews.js
+++ src/js/render-functions/renderMostViewedNews.js
@@ -9,7 +9,7 @@ export async function renderMostViewedNews({results}) {
   refs.renderContainerHome.innerHTML = '';
   let imgSrc = '/assets/actions-config-step-1.png';
   let mediaMetaData = 2;
-  
+  let alt;
   const newsMarkup = results
   .map(
     ({
@@ -20,14 +20,15 @@ export async function renderMostViewedNews({results}) {
       subsection,
       published_date,
       media,
-
+      
     }) => {
       if(media.length === 0) {
         imgSrc = '/assets/actions-config-step-1.png';
       } else {
         imgSrc = media[0]["media-metadata"][`${mediaMetaData}`].url
+        alt = media[0]["caption"]
       }
-
+      
       return `
           <li class="news__item card" data-id=${id}>
               <span class="news__category is-hidden">${subsection}</span>
@@ -35,7 +36,7 @@ export async function renderMostViewedNews({results}) {
               <div class="news__container-img">
               
               <button class="news__favorite">Add to favorite</button>
-              <img src="${imgSrc}" alt="" class="news__img"/>
+              <img src="${imgSrc}" alt="${alt}" class="news__img"/>
               </div>
               <h3 class="news__title">${title}</h3>
               <p class="news__abstract">${abstract}</p>
@@ -44,7 +45,8 @@ export async function renderMostViewedNews({results}) {
               <a href="${url}" class="news__link" target = "_blank">Read more</a>
               
           </li>
-      `;
+      `
+      
     }
   )
   .join('');
