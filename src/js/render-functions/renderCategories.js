047cfca3d0704a963fc7b8b5289c6df68d65e8b4
Roman <romeenice@gmail.com>
2023-02-19 21:38:02 +0200

    Update

--- src/js/render-functions/renderCategories.js
+++ src/js/render-functions/renderCategories.js
@@ -3,8 +3,8 @@ import { refs } from '../refs.js';
 
 export async function renderCategories() {
   const categories = await getCategories();
-  const categoriesNew = categories.slice(0, 5);
-  const categoriesNew2 = categories.slice(5, 20);
+  const categoriesNew = categories.slice(0, 6);
+  const categoriesNew2 = categories.slice(6, 20);
   const categoriesMarkup = categoriesNew
     .map(({ display_name, name }) => {
       return `
