3d927a5ce566d9c0a304dd543f06ab4010495b3c
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-20 16:59:43 +0200

    start

---
+++ src/js/pagination/favoritePagination.js
@@ -0,0 +1,16 @@
+
+//const favorite = localStorage.favorite;
+const favorite = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+const perPage = 3;
+let pages = favorite / 3;
+let page = 1;
+const paginationButon = "<button></button>";
+export function showFavoritePagination() {
+ if(favorite.length === 0) {
+    return;
+ } else {
+    let paginationMarkUp = "<button></button><button></button><button></button>"
+    
+    
+ }
+}
