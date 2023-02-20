db4e9b9593b8210ad2a7c14845069ba4b32dcbcc
Roman <romeenice@gmail.com>
2023-02-20 21:16:01 +0200

    summary
    тепер рендорить 7 + 43 на десктопі
    тепер рендорить 4+ 46 на таблетці

--- src/js/render-functions/renderCategories.js
+++ src/js/render-functions/renderCategories.js
@@ -6,16 +6,17 @@ export async function renderCategories() {
   const categories = await getCategories();
   // кагорії порізав 6 для відображення на десктопі, а остальні 14 для відображення в модалці
   const categoriesNewDesktop = categories.slice(0, 6);
-  const categoriesNew2Desktop = categories.slice(6, 20);
+  const categoriesNew2Desktop = categories.slice(6, 50);
   // порізав 20 для відображення на мобілці
-  const categoriesNewMobile = categories.slice(0, 20);
+  const categoriesNewMobile = categories.slice(0, 50);
   // порізав 4 для відображення на таблетці
   // порізав 16 для відображення на таблетці
   const categoriesNewTablet = categories.slice(0, 4);
-  const categoriesNew2Tablet = categories.slice(4, 20);
+  const categoriesNew2Tablet = categories.slice(4, 50);
   // рендер категорій на таблетці
+
   const categoriesMarupTablet = categoriesNewTablet
-    .map(({ display_name, name }) => {
+    .map(({ display_name }) => {
       return `
         <li class="categories__item">
           <button class="categories__link">${display_name}</button>
@@ -27,9 +28,10 @@ export async function renderCategories() {
     'afterbegin',
     categoriesMarupTablet
   );
+
   // рендер категорій на десктопі
   const categoriesMarkup = categoriesNewDesktop
-    .map(({ display_name, name }) => {
+    .map(({ display_name }) => {
       return `
         <li class="categories__item">
           <button class="categories__link">${display_name}</button>
@@ -43,9 +45,9 @@ export async function renderCategories() {
   );
   // рендер категорій на десктопі для модалки
   const categorieMarkup2 = categoriesNew2Desktop
-    .map(({ display_name, name }) => {
+    .map(({ display_name }) => {
       return `
-        <li class="filter-category__item">
+        <li class="filter-category__item-desktop">
           <button class="categories__link_other">${display_name}</button>
         </li>
       `;
@@ -54,5 +56,20 @@ export async function renderCategories() {
   refs.renderContainerCategoriesTheme.insertAdjacentHTML(
     'beforeend',
     categorieMarkup2
+  );
+
+  // рендер категорій на таблетці для модалки
+  const categorieMarkup2Tablet = categoriesNew2Tablet
+    .map(({ display_name, name }) => {
+      return `
+        <li class="filter-category__item-tablet">
+          <button class="categories__link_other">${display_name}</button>
+        </li>
+      `;
+    })
+    .join('');
+  refs.renderContainerCategoriesTheme.insertAdjacentHTML(
+    'beforeend',
+    categorieMarkup2Tablet
   );
 }
