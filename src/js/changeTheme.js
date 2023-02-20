dea6430eb5bda085fb9f3328d5fa051d445bf990
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-20 16:31:56 +0200

    theme changings

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -18,19 +18,34 @@ function addDarkClass() {
     if (localStorage.getItem('theme') === 'dark') {
         document.body.setAttribute('dark', '');
       swapThemeBtn.checked = true;
+      swapThemeBtn.style.background = "#4b48db";
+        swapThemeBtn.style.border = "1px solid #fff";
+        swapThemeBtn.style.borderRadius = "20px";
+        ballRef.style.backgroundColor = "#fff";
+        ballRef.style.transform = "translate(20px)";
     } else {
-        {
         document.body.removeAttribute('dark');
-        }
+        swapThemeBtn.style.background = "#fff";
+        swapThemeBtn.style.border = "1px solid #000";
+        swapThemeBtn.style.borderRadius = "20px";
+        ballRef.style.backgroundColor = "#000";
+        ballRef.style.transform = "translate(-20px)";
           }
     } catch (err) { }
 }
 
 export function addStylesForDarkThemeBtn() {
-    console.log(ballRef);
+ 
     if (localStorage.theme === 'dark') {
-        swapThemeBtn.setAttribute(style = "background:#4b48db; border: 1px solid #fff; border-radius: 20px;");
-        ballRef.setAttribute(style = "background-color: #fff; transform: translate(20px);");
+        console.log(ballRef);
+        //swapThemeBtn.setAttribute(style = "background:#4b48db; border: 1px solid #fff; border-radius: 20px;");
+        //ballRef.setAttribute(style = "background-color: #fff; transform: translate(20px);");
+
+        swapThemeBtn.style.background = "#4b48db";
+        swapThemeBtn.style.border = "1px solid #fff";
+        swapThemeBtn.style.borderRadius = "20px";
+        ballRef.style.backgroundColor = "#fff";
+        ballRef.style.transform = "translate(20px)";
                 }
 }
    
