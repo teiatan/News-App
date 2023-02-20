92fb22d2ca86c293607249550f8c1f2eafff4eca
Roman <romeenice@gmail.com>
2023-02-20 20:55:43 +0200

    коменти
    рішив добавити коменти може для себе або для когось. думаю не лишнє
    якщо треба удалю

--- src/js/render-functions/renderCategories.js
+++ src/js/render-functions/renderCategories.js
@@ -2,15 +2,18 @@ import { getCategories } from '../API/getCategories.js';
 import { refs } from '../refs.js';
 
 export async function renderCategories() {
+  // пішов взяв категорії з API
   const categories = await getCategories();
+  // кагорії порізав 6 для відображення на десктопі, а остальні 14 для відображення в модалці
   const categoriesNewDesktop = categories.slice(0, 6);
   const categoriesNew2Desktop = categories.slice(6, 20);
-
+  // порізав 20 для відображення на мобілці
   const categoriesNewMobile = categories.slice(0, 20);
-
+  // порізав 4 для відображення на таблетці
+  // порізав 16 для відображення на таблетці
   const categoriesNewTablet = categories.slice(0, 4);
   const categoriesNew2Tablet = categories.slice(4, 20);
-
+  // рендер категорій на таблетці
   const categoriesMarupTablet = categoriesNewTablet
     .map(({ display_name, name }) => {
       return `
@@ -24,7 +27,7 @@ export async function renderCategories() {
     'afterbegin',
     categoriesMarupTablet
   );
-
+  // рендер категорій на десктопі
   const categoriesMarkup = categoriesNewDesktop
     .map(({ display_name, name }) => {
       return `
@@ -38,7 +41,7 @@ export async function renderCategories() {
     'afterbegin',
     categoriesMarkup
   );
-
+  // рендер категорій на десктопі для модалки
   const categorieMarkup2 = categoriesNew2Desktop
     .map(({ display_name, name }) => {
       return `
