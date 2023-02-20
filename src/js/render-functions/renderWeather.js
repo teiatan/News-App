045524237612e0338644e23bf74ce47c123edb7b
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-02-20 13:57:22 +0200

    task40

--- src/js/render-functions/renderWeather.js
+++ src/js/render-functions/renderWeather.js
@@ -1,39 +1,39 @@
-import { format } from 'date-fns';
-import { fetchDefaultWeather, getGeoposition, fetchWeatherByGeo} from '../API/getWeather';
-export let weatherMarkup;
+// import { format } from 'date-fns';
+// import { fetchDefaultWeather, getGeoposition, fetchWeatherByGeo} from '../API/getWeather';
+// export let weatherMarkup;
 
-async function createDefaultWeatherMarkup() {
-  const data = await fetchDefaultWeather();
-  createMarkup(data);
-}
-createDefaultWeatherMarkup();
+// async function createDefaultWeatherMarkup() {
+//   const data = await fetchDefaultWeather();
+//   createMarkup(data);
+// }
+// createDefaultWeatherMarkup();
 
-export function createMarkup(data) {
-    const { temp } = data.data.main;
-    const weather = data.data.weather[0];
-    const { icon } = data.data.weather[0];
+// export function createMarkup(data) {
+//     const { temp } = data.data.main;
+//     const weather = data.data.weather[0];
+//     const { icon } = data.data.weather[0];
 
-    const weatherMarkup = `<li class="weather-item card">
-  <div class="weather-header">
-    <p>${Math.floor(temp)}</p>
-    <div>
-      <p>${weather.main}</p>
-      <p>${data.data.name}</p>
-    </div>
+//     const weatherMarkup = `<li class="weather-item card">
+//   <div class="weather-header">
+//     <p>${Math.floor(temp)}</p>
+//     <div>
+//       <p>${weather.main}</p>
+//       <p>${data.data.name}</p>
+//     </div>
 
-  </div>
-  <img class="weather-img"
-        // src="https://openweathermap.org/img/wn/${icon}@4x.png"
-        alt="Weather icon"
-        width="165"
-        height="156"/>
-  <p class="weather-day">${format(new Date(), 'eee')}</p>
-  <p class="weather-date">${format(new Date(), 'dd LLL y')}</p>
-  <a class="weather-link" href="https://sinoptik.ua" target="_blank">weather for week</a>
-</li>`;
+//   </div>
+//   <img class="weather-img"
+//         // src="https://openweathermap.org/img/wn/${icon}@4x.png"
+//         alt="Weather icon"
+//         width="165"
+//         height="156"/>
+//   <p class="weather-day">${format(new Date(), 'eee')}</p>
+//   <p class="weather-date">${format(new Date(), 'dd LLL y')}</p>
+//   <a class="weather-link" href="https://sinoptik.ua" target="_blank">weather for week</a>
+// </li>`;
 
-  // console.log(weatherMarkup);
+//   // console.log(weatherMarkup);
 
-    return weatherMarkup;
-}
+//     return weatherMarkup;
+// }
 
