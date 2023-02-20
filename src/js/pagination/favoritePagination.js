ba45e3eba07389d5ab4761b0be3574ddd7c4446b
Edouena8 <103317047+Edouena8@users.noreply.github.com>
2023-02-20 16:37:44 +0100

    Finished function

--- src/js/pagination/favoritePagination.js
+++ src/js/pagination/favoritePagination.js
@@ -1,16 +1,23 @@
-
+const paginConteinBtn = document.getElementById('pagination');
 //const favorite = localStorage.favorite;
-const favorite = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+const favorite = [ 1, 1, 1, 1, 1, 1, 1, 1, 1];
 const perPage = 3;
-let pages = favorite / 3;
-let page = 1;
-const paginationButon = "<button></button>";
+let pages = Math.ceil(favorite / perPage);
+// let page = 1;
+let paginationMarkUp = "";
+// const paginationButon = "<button></button>";
+
+
 export function showFavoritePagination() {
+
  if(favorite.length === 0) {
     return;
  } else {
-    let paginationMarkUp = "<button></button><button></button><button></button>"
-    
-    
+
+    for(let i = 1; i < pages; i += 1) {
+      paginationMarkUp + `<div><a class="page-nmb" href="#">${i}</a></div>`;
+    } 
  }
+
+ paginConteinBtn.innerHTML = paginationMarkUp;
 }
