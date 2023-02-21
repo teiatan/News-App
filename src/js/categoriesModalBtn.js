9b930e892743bb30ef2891f43b8b96b1e3e5eb10
Roman <romeenice@gmail.com>
2023-02-21 23:45:07 +0200

    сфеупщкшуі ьщвфд иет

--- src/js/categoriesModalBtn.js
+++ src/js/categoriesModalBtn.js
@@ -6,22 +6,7 @@ export default function categoriesButtonModalClick() {
   refs.renderContainerCategoriesTheme.classList.toggle('is-hidden');
   refs.modalOtherArrow.classList.toggle('switched');
 
-  refs.renderContainerCategoriesTheme.addEventListener('click', e => {
-    e.stopPropagation();
-  });
-
   // закриття модалки при кліку за межами
-
-  setTimeout(() => {
-    window.addEventListener('click', handleClickOutside);
-  }, 0);
-
-  function handleClickOutside(e) {
-    if (!refs.renderContainerCategoriesTheme.contains(e.target)) {
-      refs.modalOtherArrow.classList.remove('switched');
-      window.removeEventListener('click', handleClickOutside);
-    }
-  }
 }
 
 // refs.modalText.
