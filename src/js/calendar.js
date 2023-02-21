00f23219c818b2ed6cfb48b77e517a6bb8471653
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2023-02-22 00:28:26 +0100

    task-69
    Виправила висоту і колір днів inActive змінила (бо при ховері зливалось в одне)

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -60,7 +60,7 @@ const months = [
   'December',
 ];
 const renderCalendar = number => {
-  let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+  let firstDayofMonth = new Date(currYear, currMonth, 0).getDay(), // getting first day of month
     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
     lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
@@ -76,7 +76,7 @@ const renderCalendar = number => {
       currYear === new Date().getFullYear();
     liTag += `<li class="${isToday}">${i}</li>`;
   }
-  for (let i = lastDayofMonth; i < 6; i++) {
+  for (let i = lastDayofMonth; i < 7; i++) {
     liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
   }
   currentDate.innerText = `${months[currMonth]} ${currYear}`; 
