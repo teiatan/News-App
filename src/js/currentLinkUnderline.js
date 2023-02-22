13311c3795fff360a9d5d1df199da53f37e3ecef
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-22 02:52:53 +0200

    Nikita error

--- src/js/currentLinkUnderline.js
+++ src/js/currentLinkUnderline.js
@@ -6,7 +6,7 @@ const currentClass = 'navigation__item--current';
 
 export function currentLinkUnderline() {
     if (document.title.toLowerCase() === 'newsapp') {
-        navHomeLink.classList.add(currentClass)    
+        //navHomeLink.classList.add(currentClass)    
     } else if (document.title.toLowerCase() === 'favorite') {
         navFavoriteLink.classList.add(currentClass)
     } else if (document.title.toLowerCase() === 'read') {
