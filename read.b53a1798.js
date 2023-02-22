6ffcb6db3037136c57c18c727eb86f5ca0a1d0ea
teiatan <protas.tb@gmail.com>
2023-02-22 00:35:42 +0000

    Deploying to gh-pages from @ teiatan/News-App@5fa074aca414a5a85e24b1a871cb932366191e63 🚀

---
+++ read.b53a1798.js
@@ -0,0 +1,2 @@
+const toTop=document.querySelector(".to-top");window.addEventListener("scroll",(()=>{window.pageYOffset>100?toTop.classList.add("active"):toTop.classList.remove("active")}));
+//# sourceMappingURL=read.b53a1798.js.map
