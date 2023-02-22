4c64130f0146f6f1ce60241e6054c84f124cf26d
Roman <romeenice@gmail.com>
2023-02-22 23:16:25 +0200

    UUPPPI
    получилось отримати по категоріїї

--- src/js/API/getNewsByCategory.js
+++ src/js/API/getNewsByCategory.js
@@ -4,13 +4,13 @@ import { nytApiSettings } from '../API/nytApiSettings';
 import { categoryForSearch } from '../render-functions/renderCategories';
 
 export async function getNewsByCategory(category) {
-  const url = `${nytApiSettings.BASIC_URL}news/v3/content/all/${category}.json?api-key=${nytApiSettings.apiKey}`;
+  const url = `${nytApiSettings.BASIC_URL}search/v2/articlesearch.json?q=${category}&api-key=${nytApiSettings.apiKey}`;
 
   try {
     const response = await axios.get(url);
-    // console.log(response.data.results);
-    // console.log(url);
-    // console.log(response);
+    console.log(response.data.results);
+    console.log(url);
+    console.log(response);
     return response.data.results;
   } catch (error) {
     console.error(error);
