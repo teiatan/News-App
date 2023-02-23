3b74612c4debac5618a6d689d4c7fa5fc8a64b96
teiatan <92535482+teiatan@users.noreply.github.com>
2023-02-23 13:40:52 +0200

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,27 +1,24 @@
 export const refs = {
   body: document.body,
+
+  //хедер
   header: document.querySelector('.header'),
   burgerBtn: document.querySelector('.menu-button-burger'),
   navHome: document.querySelector('.navigation__item--home'),
   navFavorite: document.querySelector('.navigation__item--favorite'),
   navRead: document.querySelector('.navigation__item--read'),
   navWrapper: document.querySelector('.navigation'),
-  headerSignInBtn: document.querySelector('.auth-btn__sign-in'),
-  headerSignOutBtn: document.querySelector('.auth-btn__sign-out'),
-
-  renderContainerHome: document.querySelector('.render-container--home'),
-  formSearch: document.querySelector('.search-form'),
-  formSearchInput: document.querySelector('.search-form__input'),
-  negativeSearch: document.querySelector('.negative-search'),
-  developerModal: document.querySelector('.modal-developer'),
-  developerModalBtnClose: document.querySelector(".modal__btn--close"),
-  authModalBtnClose: document.querySelector('.modal__close-btn'),
 
   // відкриття/закриття модалок
   modalOpenBtns: document.querySelectorAll('[data-modal-open]'),
   modalCloseBtns: document.querySelectorAll('[data-modal-close]'),
-  // авторизація
 
+  //кнопки модалки авторизації
+  headerSignInBtn: document.querySelector('.auth-btn__sign-in'),
+  headerSignOutBtn: document.querySelector('.auth-btn__sign-out'),
+  authModalBtnClose: document.querySelector('.modal__close-btn'),
+
+  // авторизація
   authEmailInput: document.querySelector('.js-auth__email-input'),
   authPasswordInput: document.querySelector('.js-auth__password-input'),
   authEntranceBtn: document.querySelector('.js-auth__entrance-btn'),
@@ -34,20 +31,29 @@ export const refs = {
   searchForm: document.querySelector('.search-form'),
   searchFormInput: document.querySelector('.search-form__input'),
 
-  
-  
+  //форма пошуку
+  formSearch: document.querySelector('.search-form'),
+  formSearchInput: document.querySelector('.search-form__input'),
 
+  //заглушка для пошуку
+  negativeSearch: document.querySelector('.negative-search'),
+  
+  //модалка розробники
+  developerModal: document.querySelector('.modal-developer'),
+  developerModalBtnClose: document.querySelector(".modal__btn--close"),
+  
+  //контейнери для рендеру
+  renderContainerHome: document.querySelector('.render-container--home'),
   favouriteNewsContainer: document.querySelector('.render-container'),
-
   renderContainerCategoriesDesktop: document.querySelector('.categories-render-container__list-desktop'),
   renderContainerCategoriesMobile: document.querySelector('.categories-render-container__list-mobile'),
   renderContainerCategoriesTablet: document.querySelector('.categories-render-container__list-tablet'),
   renderContainerCategoriesTheme: document.querySelector('.other--categories__filters-list'),
+  renderContainerReadPage: document.querySelector('.render-container--read'),
+  navHomeLink: document.querySelector('.render-container--read'),
+  
+  //категорії
   modalText: document.querySelector('.other-categories__button-text'),
   modalOtherBtns: document.querySelector('.other-categories__button'),
   modalOtherArrow: document.querySelector('.other-categories__icon-arrow'),
-  renderContainerReadPage: document.querySelector('.render-container--read'),
-
-  navHomeLink: document.querySelector('.render-container--read'),
-
 };
